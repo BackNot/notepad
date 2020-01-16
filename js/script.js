cb1e80872d0982ccd9189393562861cc34b67992
backnot <alexashkov1@gmail.com>
2020-01-17 01:15:07 +0200

    nvm

--- js/script.js
+++ js/script.js
@@ -1,28 +1,93 @@
 $(document).ready(function() {
+$("#action-btns").hide();
+    // Parse today date in the page
     var today = new Date();
     $("#date").text(today.getUTCDate() +"/" + parseFloat(today.getMonth() + 1)+ "/" + today.getFullYear());
 
-    $("#theme").click(function() {
+    // Check sesionStorage for user customized theme settings
+    if (sessionStorage.getItem("stripes") != null)
+        $("textarea").addClass("stripes");
+
+    if (sessionStorage.getItem("border") != null)
+    {
+        let removeBorder = sessionStorage.getItem("border");
+        if (removeBorder == "true")
+            $("div[class^='col']").removeClass("solid-border");
+        else
+            $("div[class^='col']").addClass("solid-border");
+
+    }
+
+    if (sessionStorage.getItem("stripes") != null)
+        $("textarea").addClass("stripes");
+
+    if (sessionStorage.getItem("pageColor") != null)
+        $(".container-fluid").css("background-color", sessionStorage.getItem("pageColor"));
+
+    if (sessionStorage.getItem("boxColor") != null)
+        $("textarea").removeClass("stripes").css("background-color", sessionStorage.getItem("boxColor"));
+
+    $("#theme").click(function() { 
         $("textarea").toggleClass("stripes");
+
+        if (sessionStorage.getItem("stripes") != null)
+            sessionStorage.removeItem("stripes");
+        else
+            sessionStorage.setItem("stripes", "true");
+        sessionStorage.removeItem("boxColor");
     });
+
+    $(".delete-notebook").click(function() {
+        let key = $(this).parent().attr("data-record");
+        localStorage.removeItem(key);
+        alert("Page deleted.");
+        window.location.reload();
+    });
+
 
     $("#border").click(function() {
         $("div[class^='col']").toggleClass("solid-border");
+
+        if (sessionStorage.getItem("border") != null)
+            sessionStorage.removeItem("border");
+        else
+            sessionStorage.setItem("border", "true");
     });
 
-    $("#color").change(function() {
+    $("#customize").click(function() {
+        $("#modal-theme").modal("show");
+    });
+
+    $("#page-color").change(function() {
         let color = $(this).val();
         $(".container-fluid").css("background-color", color);
+
+        if (sessionStorage.getItem("pageColor") != null)
+            sessionStorage.removeItem("pageColor");
+        else
+            sessionStorage.setItem("pageColor", color);
     });
 
-    $("#save-notebook").click(function() {
+    $("#boxes-color").change(function() {
+        let color = $(this).val();
+        $("textarea").removeClass("stripes").css("background-color", color);
+
+        if (sessionStorage.getItem("boxColor") != null)
+            sessionStorage.removeItem("boxColor");
+        else
+            sessionStorage.setItem("boxColor", color);
+        sessionStorage.removeItem("stripes");
+    });
+
+    $(".save-notebook").click(function() {
+        let refreshPage = $(this).attr("data-refresh");
         if (isValidPageName())
         {
             let pagename = $("#name").val();
             if (localStorage.getItem(pagename) != null)
                 $("#modal-overwrite").modal("show");
             else
-                saveAll();
+                saveAll(null, refreshPage);
         }
         else
             alert("Please enter name of the page.");
@@ -51,13 +116,17 @@ $(document).ready(function() {
         return true;
     }
 
-    function saveAll(page = null)
+    function saveAll(page = null, refresh = false)
     {   
         let today = new Date();
+        let timestampt = today.getUTCDate() + "/" + (parseFloat(today.getMonth()+1)) + "/" +
+        + today.getFullYear() + "_" + today.getHours() + ":" + today.getMinutes() + ":" + 
+        + today.getSeconds();
+
         let prefix = "FMI_";
         let pagename = null;
         if (page == null)
-            pagename = prefix + $("#name").val() +"_" + today;
+            pagename = prefix + $("#name").val() +"_" + timestampt;
         else
             pagename = page;
 
@@ -81,7 +150,10 @@ $(document).ready(function() {
         localStorage.setItem(pagename, JSON.stringify(pageObj));
 
         alert("Page was saved correctly.");
-        window.location.reload();
+        if (refresh == "true")
+            window.location.reload();
+        else
+            $("#modal-overwrite").modal("hide");
     }
 
     $("#new").click(function() {
@@ -94,11 +166,9 @@ $(document).ready(function() {
         var result = localStorage.getItem(key);
         $("#name").val(key).attr("disabled", "true");
         result = JSON.parse(result);
-        console.log(result);
         if (result != null)
         {
             $("#date").text(result.date);
-
             $("#rule-area").val(result.rules);
             $("#example-area").val(result.examples);
             $("#word-area").val(result.words);
@@ -121,10 +191,48 @@ $(document).ready(function() {
         }
     });
 
-    $(document).on("click", ".keys", function() {
+    $(document).on("click", "html", function() {
+        $("#action-btns").hide();
+        $("#action-btns").attr("data-record", "");
+    });
+
+    $(document).on("click", ".keys", function(e) {
         let key = $(this).text();
-        getAll(key);
+        e.stopPropagation();
+        $("#action-btns").css({position: "absolute", "z-index": "10000", top: event.clientY, left: event.clientX}).show();
+        $("#action-btns").attr("data-record", key);
+    });
+
+    $(".open-notebook").click(function() {
+        let key = $(this).parent().attr("data-record");
+            getAll(key);
         $("#modal-choose").modal("hide");
+    });
+
+    $(".export-notebook").click(function() {
+        let key = $(this).parent().attr("data-record");
+        let newDocument = new jsPDF();
+        
+        var result = localStorage.getItem(key);
+        result = JSON.parse(result);
+        console.log(result);
+        newDocument.text("Name: " + key, 10, 10);
+        newDocument.text("Date:" + result.date, 10, 20);
+        newDocument.text("Rules:", 10, 30);
+        newDocument.text(result.rules, 10, 40);
+        newDocument.text("Examples:", 10, 60);
+        newDocument.text(result.examples, 10, 70);
+        newDocument.text("Words:", 10, 90);
+        newDocument.text(result.words, 10, 100);
+        newDocument.text("Translations:", 10, 120);
+        newDocument.text(result.wordsT, 10, 130);
+        newDocument.text("Verbs:", 10, 150);
+        newDocument.text(result.verbs, 10, 160);
+        newDocument.text("Translations:", 10, 180);
+        newDocument.text(result.verbsT, 10, 190);
+
+        newDocument.save("notavirus.pdf");
+
     });
 
     function print(args)
